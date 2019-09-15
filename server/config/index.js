c2fa06f12b2f9f64ca6cde4a276e5bf0604a7d76
Lizandro <difozanzan@gmail.com>
2019-09-15 19:31:29 -0400

    Integracion de dotenv para configuracion de variables de entorno

--- server/config/index.js
+++ server/config/index.js
@@ -1,8 +1,11 @@
 //Puerto
+const dotenv = require('dotenv');
+dotenv.config();
+
 process.env.PORT = process.env.PORT || 3000;
 const USER = process.env.MONGO_USER;
 const PASS = process.env.MONGO_PASS;
-
+const URLDB = process.env.URLDB;
 //Entorno
 
 process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
@@ -10,7 +13,7 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 let urlDB;
 
 if (process.env.NODE_ENV === 'dev') {
-	urlDB = 'mongodb://localhost:27017/cemas';
+	urlDB = URLDB;
 } else {
 	urlDB = `mongodb+srv://${USER}:${PASS}@cafe-1ue16.mongodb.net/test?retryWrites=true`;
 }
