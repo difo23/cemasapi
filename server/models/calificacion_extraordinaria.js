09afeac360bc799e24d4c7e9fbc92d69fb27ebe2
Pablo <pablo.mmc2010@gmail.com>
2019-11-26 13:34:23 -0400

    modelos arreglados **no usar boleano como tipo de dato

--- server/models/calificacion_extraordinaria.js
+++ server/models/calificacion_extraordinaria.js
@@ -8,8 +8,7 @@ let calificacionExtraordinariaSchema = new Schema({
 		type: String
 	},
 	estado: {
-		type: Boolean,
-		default: true
+		type: String
 	},
 	estudiante_calificado: {
 		type: String
