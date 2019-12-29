fb9956cfefeba1c133372b2286cf6e29ad6d73f8
diffo23 <lizandro23difo@gmail.com>
2019-12-29 13:43:54 -0400

    Limpieza modulos calificaciones

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,10 +2,6 @@ const express = require('express');
 const app = express();
 
 app.use(require('./estudiante'));
-app.use(require('./calificaciones_generales'));
-// app.use(require('./calif_tecnica'));
-// app.use(require('./calif_completiva'));
-// app.use(require('./calif_extraordinaria'));
 app.use(require('./profesor'));
 app.use(require('./asignaturas_academicas'));
 app.use(require('./cursos'));
