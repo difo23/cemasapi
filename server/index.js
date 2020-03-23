5f6d18347f494978345cd528a330dfc845268cd0
difo23 <diffozanzan@gmail.com>
2020-03-23 00:03:34 -0400

    Agregando catch

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 require('./config');
 const express = require('express');
-const fileUpload = require('express-fileupload')
+const fileUpload = require('express-fileupload');
 const boddyParser = require('body-parser');
 const app = express();
 const mongoose = require('mongoose');
@@ -13,10 +13,19 @@ app.use(boddyParser.json());
 app.use(require('./routes'));
 app.use(fileUpload());
 
-mongoose.connect(process.env.URLDB, { useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true }, (err, res) => {
-	if (err) throw err;
-	console.log('Mongo online');
-});
+mongoose
+	.connect(
+		process.env.URLDB,
+		{ useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true },
+		(err, res) => {
+			if (err) throw err;
+			console.log('Mongo online');
+		}
+	)
+	.then(() => console.log('DB Connected!'))
+	.catch((err) => {
+		console.log(Error, err.message);
+	});
 
 app.listen(PORT, () => {
 	console.log('Escuchando puerto', PORT);
