fe5c6ef09a19718905a210a5f63bf7ce6fb87ded
difo23 <diffozanzan@gmail.com>
2020-08-31 23:49:09 -0400

    Add codigo centro al user model

--- server/models/users.js
+++ server/models/users.js
@@ -8,6 +8,7 @@ let usersSchema = new Schema({
 
   _id: Schema.Types.ObjectId,
   username: { type: String, unique: true, required: true },
+  codigoCentro: { type: String, required: true },
   hash: { type: String, required: true },
   firstName: { type: String, required: true },
   lastName: { type: String, required: true },
